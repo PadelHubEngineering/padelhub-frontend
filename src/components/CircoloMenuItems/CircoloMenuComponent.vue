981243a2ba58a48c2663206c6610837babdb07ab
marco-zan <marco.zanon@pm.me>
2023-05-11 12:01:59 +0200

    Menu laterale con componenti
    Rimosso css di default, aggiunti i font
    Componente menu laterale circolo
    Componente item del menu
    Icona struttura

---
+++ src/components/CircoloMenuItems/CircoloMenuComponent.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+    import StructureIcon from "../icons/StructureIcon.vue"
+    import ItemWithHeaderIcon from "./ItemWithHeaderIcon.vue"
+</script>
+
+<template>
+    <div class="menuLaterale bg-bluPadelHub pt-20 font-circolo">
+
+        <ItemWithHeaderIcon
+            v-for="item in this.menu"
+            :links="item.links"
+        >
+            {{ item.title }}
+        </ItemWithHeaderIcon>
+    </div>
+</template>
+
+<script lang='ts'>
+
+    export default {
+        name: "CircoloMenuComponent",
+        data() {
+            return {
+                menu: [
+                    {
+                        title: "Struttura",
+                        links: [
+                            { nome: "gestione campi", link: "/" },
+                            { nome: "informazioni generali", link: "#" },
+                        ],
+                    },
+                    {
+                        title: "iscritti",
+                        links: [
+                            { nome: "i miei iscritti", link: "#" },
+                        ],
+                    },
+                    {
+                        title: "Statistiche",
+                        links: [
+                            { nome: "le mie statistiche", link: "#" },
+                        ],
+                    },
+                    {
+                        title: "Recensioni",
+                        links: [
+                            { nome: "le mie recensioni", link: "#" },
+                        ],
+                    },
+                ]
+
+            }
+        },
+        component: [
+            StructureIcon,
+            ItemWithHeaderIcon
+        ]
+    }
+</script>
