16f6dd684489dd37be834b885e5fb4682290408a
Matteo Gastaldello <matteo.gastaldello-1@studenti.unitn.it>
2023-05-25 17:40:17 +0200

    aggiunto campi AreaCircolo

--- src/views/AreaCircolo.vue
+++ src/views/AreaCircolo.vue
@@ -5,11 +5,14 @@
             <div class="grid grid-rows-2">
                 <div class="flex px-6">
                     <Carousel class="w-4/6 px-2" :slide=false></Carousel>
-                    <Button class="flex-auto px-2"> + </Button>
+                    <Button class="w-2/6 flex-auto px-2"> + </Button>
                 </div>
-                <div class="grid grid-cols-2 px-10 py-10 divide-x-4">
-                    <div>01</div>
-                    <div>02</div>
+                <div class="grid grid-cols-2 px-10 py-10 gap-6">
+                    <Input placeholder="Inserisci un nome" label="Nome" />
+                    <Input placeholder="Inserisci un indirizzo" label="Indirizzo" />
+                    <Input placeholder="Inserisci un telefono" label="Telefono" />
+                    <Input placeholder="Inserisci la tua E-mail" label="E-mail" />
+                    <Input placeholder="Inserisci la Partita Iva" label="Partita Iva | Codice Fiscale" />
                 </div>
             </div>
 
@@ -20,7 +23,7 @@
 
 <script setup lang="ts">
 import CircoloMenuComponent from '@/components/CircoloMenuItems/CircoloMenuComponent.vue';
-import { Carousel, Button } from 'flowbite-vue'
+import { Carousel, Button, Input } from 'flowbite-vue'
 
 
 </script>
