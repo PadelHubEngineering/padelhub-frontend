80051942353502bb50cdf961eea57c14739aa549
Matteo Gastaldello <matteo.gastaldello-1@studenti.unitn.it>
2023-06-04 18:45:05 +0200

    fixato ricerca con coordinate e ux

--- src/views/RicercaCircoli.vue
+++ src/views/RicercaCircoli.vue
@@ -1,9 +1,8 @@
 <template>
-
     <div class="sticky top-0 bg-white">
         <MobileHeader :ready="true" title='Gioca'>
             <template v-slot:rightSide>
-                <img src='/img/logoPadelHub.png' class='h-5/6'/>
+                <img src='/img/logoPadelHub.png' class='h-5/6' />
             </template>
         </MobileHeader>
 
@@ -11,17 +10,18 @@
         <p v-for="luoghi in risultati"> {{ luoghi.label }}></p> -->
         <div class="mt-8 mb-5">
             <div class="searchInput active border-2 border-bluPadelHub rounded-lg font-circolo">
-                <input type="text" placeholder="Inserire un indirizzo" v-model="input" @input="handleSuggerimenti"/>
+                <input type="text" placeholder="Inserire un indirizzo" v-model="input" @input="handleSuggerimenti" />
                 <div class="resultBox">
                     <!-- here list are inserted from javascript -->
-                    <li v-for="luoghi in risultati" :key="luoghi" @click="handleRicerca(luoghi)"> {{ luoghi.label }} </li> 
+                    <li v-for="luoghi in risultati" :key="luoghi" @click="handleRicerca(luoghi)"> {{ luoghi.label }} </li>
                 </div>
                 <div class="icon"><i class="fas fa-search"></i></div>
             </div>
             <div class="ml-10 mt-3">
                 <DataPicker @update="updateDate"></DataPicker>
             </div>
-            <button class="bg-bluPadelHub text-white rounded-lg font-circolo py-3 w-full my-5 shadow-lg" @click="ricercaCircoli">CERCA</button>
+            <button class="bg-bluPadelHub text-white rounded-lg font-circolo py-3 w-full my-5 shadow-lg disabled:bg-bluPadelHubHover"
+                @click="ricercaCircoli" :disabled="!cliccatoSuggestion">CERCA</button>
         </div>
     </div>
 
@@ -29,10 +29,10 @@
     <!-- SABRINA -->
     <div class="bg-ricercaCircoli  p-0 pt-1 bg-fixed">
         <div v-for="circolo in circoliTrovati">
-            <ItemCircoloTrovato :nomeCircolo="circolo.nomeCircolo" :iscritto="circolo.iscritto" :campi="circolo.campi"></ItemCircoloTrovato>
-        </div> 
+            <ItemCircoloTrovato :nomeCircolo="circolo.nomeCircolo" :iscritto="circolo.iscritto" :campi="circolo.campi">
+            </ItemCircoloTrovato>
+        </div>
     </div>
-
 </template>
 
 
@@ -53,86 +53,100 @@ import { useStore } from "vuex";
 const store = useStore()
 
 
-    // const provider = new OpenStreetMapProvider();
-    const searchProvider = new BingProvider({
+// const provider = new OpenStreetMapProvider();
+const searchProvider = new BingProvider({
     params: {
         key: import.meta.env.VITE_BING_MAP_KEY,
     },
-    });
+});
 
-    const circoliTrovati = [
-        {
-            nomeCircolo: "Beppone",
-            iscritto: true,
-            campi: [TipoCampo.Esterno, TipoCampo.Interno]
-        },
-        {
-            nomeCircolo: "WPadel",
-            iscritto: true,
-            campi: [TipoCampo.Esterno]
-        },
-        {
-            nomeCircolo: "InPadelWeHub",
-            iscritto: true,
-            campi: [TipoCampo.Interno]
+const circoliTrovati = [
+    {
+        nomeCircolo: "Beppone",
+        iscritto: true,
+        campi: [TipoCampo.Esterno, TipoCampo.Interno]
+    },
+    {
+        nomeCircolo: "WPadel",
+        iscritto: true,
+        campi: [TipoCampo.Esterno]
+    },
+    {
+        nomeCircolo: "InPadelWeHub",
+        iscritto: true,
+        campi: [TipoCampo.Interno]
 
-        }
-    ]
-
-
-    let input = ref()
-    let risultati = reactive({})
-    let isFree = 0;
-    let data:string;
-
-    async function updateDate(dataValue:Date) {
-        let mese = dataValue.getMonth() + 1
-        data = dataValue.getFullYear() + "/" + mese.toString().padStart(2, '0') + "/" + (dataValue.getDate()).toString().padStart(2, '0') 
     }
+]
 
-    async function handleSuggerimenti() {
-        if (isFree <= 20) {
-            isFree++
-            const results = await searchProvider.search({ query: input.value });
-            Object.assign(risultati, results)
-            isFree--
-        }
-        console.log(risultati)
+
+let input = ref()
+let risultati = reactive({})
+let cliccatoSuggestion = ref(false)
+let isFree = 0;
+let data: string;
+let location = {
+    x: null,
+    y: null
+}
+
+async function updateDate(dataValue: Date) {
+    let mese = dataValue.getMonth() + 1
+    data = dataValue.getFullYear() + "/" + mese.toString().padStart(2, '0') + "/" + (dataValue.getDate()).toString().padStart(2, '0')
+}
+
+async function handleSuggerimenti() {
+    cliccatoSuggestion.value = false
+    if (isFree <= 20) {
+        isFree++
+        const results = await searchProvider.search({ query: input.value });
+        Object.assign(risultati, results)
+        isFree--
     }
-    async function handleRicerca(key: any) {
-        console.log(key)
-        input.value = key.label
-    }
+    //console.log(risultati)
+}
+async function handleRicerca(key: any) {
+    console.log(key)
+    input.value = key.label
+    location.x = key.x
+    location.y = key.y
+    cliccatoSuggestion.value = true
+    risultati = reactive({})
+}
 
-    async function ricercaCircoli(){
-        console.log("Luogo: "+ input.value +  ", Data: " + data)
+async function ricercaCircoli() {
+    console.log("Luogo: " + input.value + ", Data: " + data)
 
-        if (!axios) return
+    if (!axios) return
 
-        let response: AxiosResponse | undefined;
+    let response: AxiosResponse | undefined;
 
-        try{
+    if (location.x != null && location.y != null) {
+        try {
             response = await axios.get(
-                `${import.meta.env.VITE_BACK_URL}/api/v1/ricercaCircoli?luogo=${input.value}&data=${data}`, //Impostare l'URL a cui collagarsi
+                `${import.meta.env.VITE_BACK_URL}/api/v1/ricercaCircoli?x=${location.x}&y=${location.y}&data=${data}`, //Impostare l'URL a cui collagarsi
                 {
                     headers: {
                         'x-access-token': store.state.auth.token
                     }
                 }
             )
-
-            
-
-        } catch( err: any ) {
+        } catch (err: any) {
             console.log(err)
         }
-
-        console.log(response)
-
+    }
+    else{
+        console.log("Devi cliccare sulle suggestion!");
     }
 
-    onMounted(()=>{
-        updateDate(new Date())
-    })
+
+
+    console.log(response)
+
+}
+
+onMounted(() => {
+    updateDate(new Date())
+})
 
 </script>
